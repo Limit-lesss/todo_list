00ecb1d88b2b7352357b6ad0b083ded7fca840e4
Pranav Kumar <engineersempire0204@gmail.com>
2024-01-18 16:07:00 +0530

    update css

--- script.js
+++ script.js
@@ -3,46 +3,47 @@ let inputBox = document.querySelector(".add_list");
 let listBox = document.querySelector(".list");
 inputBox.value = "";
 
-addBtn.addEventListener("click",() =>{
+addBtn.addEventListener("click", () => {
+  addList();
+});
+inputBox.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
     addList();
-});
-inputBox.addEventListener("keypress",(event) =>{
-    if(event.key == "Enter"){
-        addList();
-    } 
+  }
 });
 
-function addList(){
-    if(inputBox.value == ""){
-        alert("You must write something!");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value ;
-        listBox.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
+function addList() {
+  if (inputBox.value == "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    let p = document.createElement("p");
+    p.innerHTML = inputBox.value;
+    li.appendChild(p);
+    listBox.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  saveData();
+  inputBox.value = "";
+}
+
+listBox.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
     saveData();
-    inputBox.value = "";
-}
-
-listBox.addEventListener("click",function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
+  }
+  if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
 });
 
-function saveData(){
-    localStorage.setItem("data",listBox.innerHTML);
+function saveData() {
+  localStorage.setItem("data", listBox.innerHTML);
 }
-function showTask(){
-    listBox.innerHTML = localStorage.getItem("data");
+function showTask() {
+  listBox.innerHTML = localStorage.getItem("data");
 }
 showTask();
